606c28809ebd3f66c31b25b89ba89e623d50c896
cletushunsu <cletusola@outlook.com>
2024-05-16 06:01:45 -0400

    Added user and profile url path

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import SignUpRequest, SignInRequest, SignOutRequest 
+from .views import(
+    SignUpRequest, SignInRequest, SignOutRequest,
+    ProfileRequest, ProfileDetailsRequest,
+    UserRequest, UserDetailsRequest
+)
 from rest_framework_simplejwt.views import TokenRefreshView 
 
 
@@ -11,5 +15,10 @@ urlpatterns = [
 	path('signin/', SignInRequest.as_view(), name='signin'), # user sign in view url
 	path('signout/', SignOutRequest.as_view(), name='signout'), # user sign out url
 	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Token refresh url
+    path('profile/', ProfileRequest.as_view(), name='profile'), # user profile url path
+    path('profile_details/<int:pk>/', ProfileDetailsRequest.as_view(), name='profile_details'),  # profile details url path
+    path('user/', UserRequest.as_view(), name='user'), # user info url path
+    path('user_details/<int:pk>/', UserDetailsRequest.as_view(), name='user_details'), # user details url path
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
