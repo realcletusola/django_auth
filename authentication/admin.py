d81ac06090ba488e8d3ea0a958b9f41341977b88
cletushunsu <cletusola@outlook.com>
2024-05-16 02:44:48 -0400

    created user and profile admin classes

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser, UserProfile 
+
+
+class CustomeUserAdmin(UserAdmin): # custom user admin display 
+
+    model = CustomUser
+
+    list_display = ['username', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login']
+    list_display_links = ['username', 'email']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'last_login']
+    fieldsets = [
+        ('Basic Info', {'fields':('username', 'email', 'password')}),
+        ('Permissions', {'fields':('is_active','is_staff','is_superuser',
+                        'groups','user_permissions')}),
+        ('Dates', {'fields': ('last_login',)})
+    ]
+    ordering = ('username', 'email',)
+    search_fields = ['username', 'email']
+
+admin.site.register(CustomUser,CustomeUserAdmin)
+
+
+class UserAdmin(admin.ModelAdmin): # user profile admin
+    list_display = ['display_name','user','login_trials','max_login_trials','time']
+    list_display_links = ['display_name','user','time']
+
+admin.site.register(UserProfile,UserAdmin)
