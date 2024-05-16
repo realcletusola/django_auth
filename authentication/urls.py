035a51db9e9c0c24e740a071875642032273f179
cletushunsu <cletusola@outlook.com>
2024-05-16 05:52:58 -0400

    Imported settings and statc modules for media path implementation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import SignUpRequest, SignInRequest, SignOutRequest 
 from rest_framework_simplejwt.views import TokenRefreshView 
+
 
 urlpatterns = [
 	path('signup/', SignUpRequest.as_view(), name='signup'), # user sign up url
 	path('signin/', SignInRequest.as_view(), name='signin'), # user sign in view url
 	path('signout/', SignOutRequest.as_view(), name='signout'), # user sign out url
 	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Token refresh url
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
